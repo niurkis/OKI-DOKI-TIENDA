fc724b62fc02863c5905b6b84810a09b26e35e0b
Niurka Mejia <niurka.mejia612@pascualbravo.edu.co>
2026-05-13 20:21:05 -0500

    Diseño Oki Doki negro y plata con rutas corregidas

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const path = require("path");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// 1. CARPETA PÚBLICA (Aquí es donde Express buscará /css/style.css)
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/views", express.static(path.join(__dirname, "views")));
 
-app.use("/api/productos", require("./routes/productos"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacto", require("./routes/contacto"));
-
+// 2. RUTAS DE LAS VISTAS (Páginas principales)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
@@ -24,17 +22,22 @@ app.get("/carrito", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "carrito.html"));
 });
 
-app.get("/producto/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "producto.html"));
+app.get("/nosotros", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "nosotros.html"));
 });
 
 app.get("/contacto", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "contacto.html"));
 });
 
-app.get("/nosotros", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "nosotros.html"));
+app.get("/producto/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "producto.html"));
 });
+
+// 3. RUTAS DE LA API
+app.use("/api/productos", require("./routes/productos"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacto", require("./routes/contacto"));
 
 const PORT = 3000;
 app.listen(PORT, () => {
