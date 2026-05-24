a025d9337dd482f988558645633f525d864b8447
Niurka Mejia <niurka.mejia612@pascualbravo.edu.co>
2026-05-24 12:09:01 -0500

    Ingreso de imagenes

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "okidokibd1",
+  database: process.env.DB_NAME || "okidokibd",
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "okidokibd1",
+  database: process.env.DB_NAME || "okidokibd",
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
